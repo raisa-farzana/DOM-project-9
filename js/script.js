dcfc568813f0a23236f7787ebf04bbbfa3c1c12c
raisa-farzana <raisafarzana5502@gmail.com>
2025-04-08 18:44:05 +0600

    update html css js file

--- js/script.js
+++ js/script.js
@@ -1,9 +1,29 @@
+let currentIndex = 0;
+const showSlide = (index) => {
+    const slides = document.getElementById('slides');
+    const totalSlides = slides.children.length;
+
+    // if(index >= totalSlides){
+    //     currentIndex = 0;
+    // }
+    // else if(index < 0){
+    //     currentIndex = totalSlides - 1
+    // }
+    // else{
+    //     currentIndex = index;
+    // }
+
+    currentIndex = (index + totalSlides) % totalSlides;
+
+    slides.style.transform = `translateX(${-currentIndex * 100}%)`
+}
+
 const prevSlide = () => {
-    console.log('hye');
+    showSlide(currentIndex - 1);
     
 }
 
 const nextSlide = () => {
-    console.log('hello');
+    showSlide(currentIndex + 1);
     
 }
